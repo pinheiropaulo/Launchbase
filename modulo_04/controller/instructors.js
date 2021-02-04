4ed12c5de57e47196c6192743308bf509568b972
Paulo Pinheiro <pauloprap57@gmail.com>
2021-02-04 12:54:12 -0300

    Membros

--- modulo_04/controller/instructors.js
+++ modulo_04/controller/instructors.js
@@ -1,41 +1,15 @@
 const fs = require("fs");
 
-const { age, date } = require("./utils");
+const { age, date } = require("../utils");
 
-const data = require("./data.json");
+const data = require("../data.json");
 
 exports.index = (req, res) => {
   return res.render("instructors/index", { instructors: data.instructors });
 };
 
-exports.show = (req, res) => {
-  const { id } = req.params;
-
-  const foundInstruction = data.instructors.find((instructor) => {
-    return id == instructor.id;
-  });
-
-  if (!foundInstruction) return res.send("Instructor Not Found");
-
-  const verifyGender = () => {
-    if (foundInstruction.gender == "M") {
-      return "Masculino";
-    } else {
-      return "Feminino";
-    }
-  };
-
-  const instructor = {
-    ...foundInstruction,
-    age: age(foundInstruction.birth),
-    services: foundInstruction.services.split(","),
-    created_at: new Intl.DateTimeFormat("pt-BR").format(
-      foundInstruction.created_at
-    ),
-    gender: verifyGender(),
-  };
-
-  return res.render("instructors/show", { instructor });
+exports.create = (req, res) => {
+  return res.render("instructors/create");
 };
 
 exports.post = (req, res) => {
@@ -66,22 +40,52 @@ exports.post = (req, res) => {
   fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
     if (err) return res.send("Erro");
 
-    return res.redirect("/instructors");
+    return res.redirect(`/instructors/${id}`);
   });
+};
+
+exports.show = (req, res) => {
+  const { id } = req.params;
+
+  const foundInstructor = data.instructors.find((instructor) => {
+    return id == instructor.id;
+  });
+
+  if (!foundInstructor) return res.send("Instructor Not Found");
+
+  const verifyGender = () => {
+    if (foundInstructor.gender == "M") {
+      return "Masculino";
+    } else {
+      return "Feminino";
+    }
+  };
+
+  const instructor = {
+    ...foundInstructor,
+    age: age(foundInstructor.birth),
+    services: foundInstructor.services.split(","),
+    created_at: new Intl.DateTimeFormat("pt-BR").format(
+      foundInstructor.created_at
+    ),
+    gender: verifyGender(),
+  };
+
+  return res.render("instructors/show", { instructor });
 };
 
 exports.edit = (req, res) => {
   const { id } = req.params;
 
-  const foundInstruction = data.instructors.find((instructor) => {
+  const foundInstructor = data.instructors.find((instructor) => {
     return id == instructor.id;
   });
 
-  if (!foundInstruction) return res.send("Instructor Not Found");
+  if (!foundInstructor) return res.send("Instructor Not Found");
 
   const instructor = {
-    ...foundInstruction,
-    birth: date(foundInstruction.birth),
+    ...foundInstructor,
+    birth: date(foundInstructor.birth).iso,
   };
 
   return res.render("instructors/edit", { instructor });
@@ -91,17 +95,17 @@ exports.put = (req, res) => {
   const { id } = req.body;
   let index = 0;
 
-  const foundInstruction = data.instructors.find((instructor, foundIndex) => {
+  const foundInstructor = data.instructors.find((instructor, foundIndex) => {
     if (id == instructor.id) {
       index = foundIndex;
       return true;
     }
   });
 
-  if (!foundInstruction) return res.send("Instructor Not Found");
+  if (!foundInstructor) return res.send("Instructor Not Found");
 
   const instructor = {
-    ...foundInstruction,
+    ...foundInstructor,
     ...req.body,
     birth: Date.parse(req.body.birth),
     id: Number(req.body.id),
@@ -119,11 +123,11 @@ exports.put = (req, res) => {
 exports.delete = (req, res) => {
   const { id } = req.body;
 
-  const filteredInstructors = data.instructors.filter((instructor) => {
+  const foundInstructor = data.instructors.filter((instructor) => {
     return instructor.id != id;
   });
 
-  data.instructors = filteredInstructors;
+  data.instructors = foundInstructor;
 
   fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
     if (err) return res.send("Erro");
