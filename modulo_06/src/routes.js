6dc268c4f6d71a660316508fc6ff5e9c3dd01e3d
Paulo Pinheiro <pauloprap57@gmail.com>
2021-11-27 02:34:46 -0300

    Página de Busca

--- modulo_06/src/routes.js
+++ modulo_06/src/routes.js
@@ -4,11 +4,17 @@ import multer from './app/middlewares/multer';
 
 import ProductController from './app/controller/ProductController';
 import HomeController from './app/controller/HomeController';
+import SearchController from './app/controller/SearchController';
 
 const routes = Router();
 
+// Home
 routes.get('/', HomeController.index);
 
+// Search
+routes.get('/products/search', SearchController.index);
+
+// Products
 routes.get('/products/create', ProductController.create);
 routes.get('/products/:id', ProductController.show);
 routes.get('/products/:id/edit', ProductController.edit);
